aeb290aac1750cf36bf431de009e4c536e8fbea5
WindowsHyun <kwon616@gmail.com>
2016-05-31 00:16:23 +0900

    파싱 함수, tm좌표, 문자열자르기
    위경도 받아와서 tm좌표로 변환하여 근처 측정소 찾기.
    파싱 하는 부분을 함수로 만들어서 코드를 최소화
    문자열 자르기를 따로 만들어서 좀더 편하게 작업함

--- miniDust/FunctionData.swift
+++ miniDust/FunctionData.swift
@@ -30,39 +30,111 @@ class MyLocation {
 }
 
 
-class parsingData{
+class parsingData: NSObject, NSXMLParserDelegate{
     var parser = NSXMLParser()
     // feed 데이터를 저장하는 mutable array
     var posts = NSMutableArray()
     // title과 date 같은 feed데이터를 저장하는 mutable dictionary
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var parsingData = NSMutableString()
+    var noDataValue = false
+    var motherValue: String = ""
+    var ChildValue: String = ""
+    var returnData:String = ""
+    
+    func beginParsing(url : String, motherData : String, ChildData : String) -> String{
+        posts = []
+        motherValue = motherData
+        ChildValue = ChildData
+        parser = NSXMLParser(contentsOfURL: (NSURL(string:url))!)!
+        parser.delegate = self
+        parser.parse()
+        if ( posts.count != 0){
+            returnData = posts.objectAtIndex(0).valueForKey(ChildValue) as! NSString as String
+            returnData = returnData.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+            return returnData
+        }else{
+            return "-"
+        }
+    }
+    
+    
+    func parser(parser:NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?,
+                attributes attributeDict: [String : String]) {
+        element = elementName
+        if(elementName as NSString).isEqualToString(motherValue){
+            noDataValue = true
+            // element 가 item이면 아래를 실행
+            elements = NSMutableDictionary()
+            elements = [:]
+            parsingData = NSMutableString()
+            parsingData = ""
+            
+        }
+    }
+    
+    func parser(parser: NSXMLParser!, foundCharacters string: String!){
+        if element.isEqualToString(ChildValue){
+            parsingData.appendString(string)
+        }
+    }
+    
+    
+    func parser(parser: NSXMLParser!, didEndElement elementName: String!,namespaceURI: String!, qualifiedName qName: String!){
+        if(elementName as NSString).isEqualToString(motherValue){
+            if !parsingData.isEqual(nil){
+                elements.setObject(parsingData, forKey: ChildValue)
+            }
+            posts.addObject(elements)
+        }
+        
+    }
+    
 }
 
 
+func SplitString(data: String, first : String, last : String) -> String {
+    let firstSplit = data.componentsSeparatedByString(first)
+    let lastSplit = firstSplit[0].componentsSeparatedByString(last)
+    return String(lastSplit[1])
+}
 
-
-/*
-class pm10Data {
-    static let sharedInstance = pm10Data()
-    var dateTime: String
-    var so2Value: String
-    var coValue: String
-    var o3Value: String
-    var no2Value: String
-    var pm10Value: String
-    var khaiValue: String
+func WebParsing(url : String) -> String {
+    /*
+     비동기식 파싱을 하면 로딩 중에 사용을 할수 있다 하지만
+     비동기식으로 할경우 reguest값을 추출해낼수가 없다?
+     그래서 동기식으로 작업을 한다.
+     */
+    let url = NSURL(string: url)
+    let cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData
+    var request = NSMutableURLRequest(URL: url!, cachePolicy: cachePolicy, timeoutInterval: 2.0)
+    request.HTTPMethod = "GET"
+    // set data
+    var dataString = ""
+    let requestBodyData = (dataString as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+    request.HTTPBody = requestBodyData
     
-    private init() {
-        self.dateTime = ""
-        self.so2Value = ""
-        self.coValue = ""
-        self.o3Value = ""
-        self.no2Value = ""
-        self.pm10Value = ""
-        self.khaiValue = ""
+    var response: NSURLResponse? = nil
+    var reponseError: NSError?
+    var reply:NSData?
+    
+    do {
+        reply = try NSURLConnection.sendSynchronousRequest(request, returningResponse:&response)
+    } catch let error as NSError {
+        reponseError = error
+        reply = nil
+    }
+    
+    let results = NSString(data:reply!, encoding:NSUTF8StringEncoding) as! String
+    if (reply != nil){
+        return results.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+    }else{
+        return "-"
     }
 }
-*/
+
+
 func UTF8Encode(name: String) -> String {
-    
     return name.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
 }
--- miniDust/miniDustViewController.swift
+++ miniDust/miniDustViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
 class miniDustViewController: UIViewController, NSXMLParserDelegate {
-
+    
     @IBOutlet var myLocation : UILabel!
     @IBOutlet var dataTime : UILabel!
     @IBOutlet var khailValue : UILabel!
@@ -26,116 +26,14 @@ class miniDustViewController: UIViewController, NSXMLParserDelegate {
     }
     
     //-----------------------------------------------------------------------------
-    // 파싱을 위한 변수 만들기
-    @IBOutlet weak var tbData: UITableView!
-    // xml 파일을 다운로드 및 파싱하는 오브젝트
-    var parser = NSXMLParser()
-    // feed 데이터를 저장하는 mutable array
-    var posts = NSMutableArray()
-    // title과 date 같은 feed데이터를 저장하는 mutable dictionary
-    var elements = NSMutableDictionary()
-    var element = NSString()
-    var parsingTime = NSMutableString()
-    var parsingKhaiValue = NSMutableString()
-    var parsingso2Value = NSMutableString()
-    var parsingcoValue = NSMutableString()
-    var parsingno2Value = NSMutableString()
-    var parsingo3Value = NSMutableString()
-    var parsingpm10Value24Data = NSMutableString()
+    var siteURL:String = ""
+    var encodeName:String = ""
     var noDataValue = false
+    var test:String = ""
     //-----------------------------------------------------------------------------
     
     let LocationData = MyLocation.sharedInstance
-    
-    var encodeName:String = ""
-    
-    func beginParsing(){
-        posts = []
-        encodeName = UTF8Encode(LocationData.ThirdLocation)
-        parser = NSXMLParser(contentsOfURL: (NSURL(string:"http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?ServiceKey=0XhuR6MWTmnuwV8IZ3UntOteG%2BqKHXdHVDCtFHQu3Y67n4Wwwuc6zCOj%2Bk0TrBN6bvqswhm2BtIvs6sP8%2FQruA%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1&stationName=\(encodeName)&dataTerm=DAILY"))!)!
-        parser.delegate = self
-        parser.parse()
-    }
-    
-    func parser(parser:NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?,
-                attributes attributeDict: [String : String]) {
-        element = elementName
-        if(elementName as NSString).isEqualToString("item"){
-            noDataValue = true
-            // element 가 item이면 아래를 실행
-            elements = NSMutableDictionary()
-            elements = [:]
-            parsingTime = NSMutableString()
-            parsingTime = ""
-            parsingKhaiValue = NSMutableString()
-            parsingKhaiValue = ""
-            parsingso2Value = NSMutableString()
-            parsingso2Value = ""
-            parsingcoValue = NSMutableString()
-            parsingcoValue = ""
-            parsingno2Value = NSMutableString()
-            parsingno2Value = ""
-            parsingo3Value = NSMutableString()
-            parsingo3Value = ""
-            parsingpm10Value24Data = NSMutableString()
-            parsingpm10Value24Data = ""
-            
-        }
-    }
-    
-    func parser(parser: NSXMLParser!, foundCharacters string: String!){
-        if element.isEqualToString("dataTime"){
-            parsingTime.appendString(string)
-        }
-        if element.isEqualToString("khaiValue"){
-            parsingKhaiValue.appendString(string)
-        }
-        if element.isEqualToString("pm10Value"){
-            parsingpm10Value24Data.appendString(string)
-        }
-        if element.isEqualToString("o3Value"){
-            parsingo3Value.appendString(string)
-        }
-        if element.isEqualToString("no2Value"){
-            parsingno2Value.appendString(string)
-        }
-        if element.isEqualToString("coValue"){
-            parsingcoValue.appendString(string)
-        }
-        if element.isEqualToString("so2Value"){
-            parsingso2Value.appendString(string)
-        }
-    }
-    
-    
-    func parser(parser: NSXMLParser!, didEndElement elementName: String!,namespaceURI: String!, qualifiedName qName: String!){
-        if(elementName as NSString).isEqualToString("item"){
-            if !parsingTime.isEqual(nil){
-                elements.setObject(parsingTime, forKey: "dataTime")
-            }
-            if !parsingKhaiValue.isEqual(nil){
-                elements.setObject(parsingKhaiValue, forKey: "khaiValue")
-            }
-            if !parsingpm10Value24Data.isEqual(nil){
-                elements.setObject(parsingpm10Value24Data, forKey: "pm10Value")
-            }
-            if !parsingo3Value.isEqual(nil){
-                elements.setObject(parsingo3Value, forKey: "o3Value")
-            }
-            if !parsingno2Value.isEqual(nil){
-                elements.setObject(parsingno2Value, forKey: "no2Value")
-            }
-            if !parsingcoValue.isEqual(nil){
-                elements.setObject(parsingcoValue, forKey: "coValue")
-            }
-            if !parsingso2Value.isEqual(nil){
-                elements.setObject(parsingso2Value, forKey: "so2Value")
-            }
-            posts.addObject(elements)
-        }
-        
-    }
-    
+    let GetParsing = parsingData()
     
     
     func update() {
@@ -144,89 +42,47 @@ class miniDustViewController: UIViewController, NSXMLParserDelegate {
     }
     
     func pm10Update() {
-        // 실시간으로 초미세먼지 데이터를 받아온다.
-        beginParsing()
-        print("파싱을 시작함..!")
-        if ( posts.count != 0){
-        // 파싱한 데이터가 있을경우에만 작성해준다..!
-        dataTime.text = posts.objectAtIndex(0).valueForKey("dataTime") as! NSString as String
-        // 시간값을 가져와 dataTime에 넣어준다.
-            
-        var khai = posts.objectAtIndex(0).valueForKey("khaiValue") as! NSString as String
-        khai = khai.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-        khailValue.text = "통합지수 : " + String(khai) + String("㎍/㎥")
-        // 통합지수를 가져와서 뿌려주는데 통합지수 뒤에 공백과 줄구분이 있어 문자열 자르기를 이요하여 자른후 붙어넣기 한다.
-            
-        var pm10 = posts.objectAtIndex(0).valueForKey("pm10Value") as! NSString as String
-        pm10 = pm10.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-        pm10Value24Data.text = String(pm10) + String("㎍/㎥")
-            
-        var o3 = posts.objectAtIndex(0).valueForKey("o3Value") as! NSString as String
-        o3 = o3.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-        o3Value.text = String(o3) + String("ppm")
-            
-        var no2 = posts.objectAtIndex(0).valueForKey("no2Value") as! NSString as String
-        no2 = no2.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-        no2Value.text = String(no2) + String("ppm")
-            
-        var co = posts.objectAtIndex(0).valueForKey("coValue") as! NSString as String
-        co = co.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-        coValue.text = String(co) + String("ppm")
-            
-        var so2 = posts.objectAtIndex(0).valueForKey("so2Value") as! NSString as String
-        so2 = so2.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-        so2Value.text = String(so2) + String("ppm")
-            
+        print("파싱을 시작전 현재 위치를 기준으로 가까운 위치를 찾는다..!")
+        
+        siteURL = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList?ServiceKey=agRTEvpQv1bNvtoPQr3DNvE5juZ9EAws47JkmLbQnf4OYYAXw%2FAh9TULJtGxrEBzqH2767koxGlukyRTjweQcg%3D%3D&tmX=\(LocationData.TM_x)&tmY=\(LocationData.TM_y)&numOfRows=999&pageSize=999&pageNo=1&startPage=1"
+        
+        var nearAir = GetParsing.beginParsing(siteURL, motherData: "item", ChildData: "stationName")
+        LocationData.ThirdLocation = String(nearAir)
+        
+        print("이제 파싱을 시작한다..!")
+        encodeName = UTF8Encode(LocationData.ThirdLocation)
+        siteURL = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?ServiceKey=0XhuR6MWTmnuwV8IZ3UntOteG%2BqKHXdHVDCtFHQu3Y67n4Wwwuc6zCOj%2Bk0TrBN6bvqswhm2BtIvs6sP8%2FQruA%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1&stationName=\(encodeName)&dataTerm=DAILY"
+        // 파싱을 하기위한 URL등을 미리 입력해주어 중복된 값들을 하나로 통일 시킨다.
+        dataTime.text = GetParsing.beginParsing(siteURL, motherData: "item", ChildData: "dataTime")
+        khailValue.text = "통합지수 : " + GetParsing.beginParsing(siteURL, motherData: "item", ChildData: "khaiValue") + String("㎍/㎥")
+        pm10Value24Data.text = GetParsing.beginParsing(siteURL, motherData: "item", ChildData: "pm10Value") + String("㎍/㎥")
+        o3Value.text = GetParsing.beginParsing(siteURL, motherData: "item", ChildData: "o3Value") + String("ppm")
+        no2Value.text = GetParsing.beginParsing(siteURL, motherData: "item", ChildData: "no2Value") + String("ppm")
+        coValue.text = GetParsing.beginParsing(siteURL, motherData: "item", ChildData: "coValue") + String("ppm")
+        so2Value.text = GetParsing.beginParsing(siteURL, motherData: "item", ChildData: "so2Value") + String("ppm")
         print("파싱 데이터를 뿌림.")
-        }else{
-        print("No Data")
-        dataTime.text = "No Data"
-        khailValue.text = "통합지수 : " + "-" + String("㎍/㎥")
-        pm10Value24Data.text = "-" + String("㎍/㎥")
-        o3Value.text = "-" + String("ppm")
-        no2Value.text = "-" + String("ppm")
-        coValue.text = "-" + String("ppm")
-        so2Value.text = "-" + String("ppm")
-            
-        }
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "update", userInfo: nil, repeats: true)
-        var pm10Timer = NSTimer.scheduledTimerWithTimeInterval(3600.0, target: self, selector: "pm10Update", userInfo: nil, repeats: true)
+        
+        var recData:String = WebParsing("https://apis.daum.net/local/geo/transcoord?apikey=d8807fd4a736291f4878c3cd37d8612d&fromCoord=WGS84&y=\(LocationData.WGS84_x)&x=\(LocationData.WGS84_y)&toCoord=TM&output=xml")
+        
+        LocationData.TM_x = Double(SplitString(recData, first: "' y", last: "x='"))!
+        LocationData.TM_y = Double(SplitString(recData, first: "' />", last: "y='"))!
+        print("TM 좌표 : " + String(LocationData.TM_x) + ", " + String(LocationData.TM_y) )
+        
+        let timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "update", userInfo: nil, repeats: true)
+        let pm10Timer = NSTimer.scheduledTimerWithTimeInterval(3600.0, target: self, selector: "pm10Update", userInfo: nil, repeats: true)
         pm10Update()
-        
-        
-        
-        
-        var urlstring = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?ServiceKey=agRTEvpQv1bNvtoPQr3DNvE5juZ9EAws47JkmLbQnf4OYYAXw%2FAh9TULJtGxrEBzqH2767koxGlukyRTjweQcg%3D%3D&numOfRows=999&pageSize=999&pageNo=1&startPage=1&sidoName=%EA%B2%BD%EA%B8%B0"
-        var url = NSURL(string: urlstring)
-        var postData:String = ""
-        var request = NSMutableURLRequest(URL: url!)
-        request.HTTPMethod = "GET"
-        request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        request.HTTPBody = postData.dataUsingEncoding(NSUTF8StringEncoding)
-        var connection = NSURLConnection(request: request, delegate: nil, startImmediately: true)
-        
-        print(request.HTTPBody)
-        
-    }
+        //print(String(LocationData.WGS84_x) + String(", ") + String(LocationData.WGS84_y))
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
-
+    
+    
 }
